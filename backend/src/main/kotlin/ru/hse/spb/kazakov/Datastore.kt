45d6ad401d3d8744dedc709a3cff3530e8c851d5
Dmitry <qudeed@gmail.com>
2018-11-14 00:44:25 +0300

    Set server ip

--- backend/src/main/kotlin/ru/hse/spb/kazakov/Datastore.kt
+++ backend/src/main/kotlin/ru/hse/spb/kazakov/Datastore.kt
@@ -5,7 +5,7 @@ import org.mongodb.morphia.Datastore
 import org.mongodb.morphia.Morphia
 
 object Datastore {
-    const val HOST = "127.0.0.1"
+    const val HOST = "104.248.174.165"
     const val PORT = 27017
     const val DB_NAME = "BookTravelGuide"
     val instance: Datastore
